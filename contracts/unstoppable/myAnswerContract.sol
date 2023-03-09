404a6ad0353d379da92e57b64544f7966935547c
DiazRock <corolariodiaz@gmail.com>
2023-03-09 14:37:00 -0500

    Solution for unstoppable

---
+++ contracts/unstoppable/myAnswerContract.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "@openzeppelin/contracts/interfaces/IERC3156FlashBorrower.sol";
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+
+import "./ReceiverUnstoppable.sol";
+import "./UnstoppableVault.sol";
+import "../DamnValuableToken.sol";
+import "./UnstoppableVault.sol";
